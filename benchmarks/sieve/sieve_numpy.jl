94fc07e634dc5c02eeb2c370ef361d9cb5cb94e0
MiguelMarcelino <mac.marcelino@outlook.pt>
2022-09-13 17:50:08 +0100

    Improved sieve numpy translation

--- benchmarks/sieve/sieve_numpy.jl
+++ benchmarks/sieve/sieve_numpy.jl
@@ -1,12 +1,12 @@
 function sieve(n)
-    primes = ones(Bool, n)
+    primes = trues(n)
     (primes[1], primes[2]) = (false, false)
     for i = 2:Int(floor(sqrt(n) + 1))-1
         if primes[i+1]
             primes[i*i+1:i:end] .= false
         end
     end
-    return [i for i in (0:length(primes)-1) if primes[i+1] != 0]
+    return [i-1 for (i,p) in enumerate(primes) if p != 0]
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
