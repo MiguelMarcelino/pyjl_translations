44ac3737b507f86f4e3a008f1fdcbb59b1466d9a
MiguelMarcelino <mac.marcelino@outlook.pt>
2022-10-12 21:07:56 +0100

    Minor changes

--- neural-network/network-py2many/network.jl
+++ neural-network/network-py2many/network.jl
@@ -1,7 +1,6 @@
 #= 
 network.py
 ~~~~~~~~~~
-IT WORKS
 
 A module to implement the stochastic gradient descent learning
 algorithm for a feedforward neural network.  Gradients are calculated
--- neural-network/network/network.py
+++ neural-network/network/network.py
@@ -3,7 +3,6 @@
 """
 network.py
 ~~~~~~~~~~
-IT WORKS
 
 A module to implement the stochastic gradient descent learning
 algorithm for a feedforward neural network.  Gradients are calculated
